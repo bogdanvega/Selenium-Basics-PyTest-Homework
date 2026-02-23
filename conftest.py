27f7bc878ddf439792cbdf617df5668d63c2e2b9
v3gas90b <v3gas90b@gmail.com>
2026-02-23 12:11:29 +0100

    updated conftest.py to handle google password manager pop-up by starting the chrome session as a guest user and delete all cookies to have a clean session everytime

--- conftest.py
+++ conftest.py
@@ -8,9 +8,13 @@ def selenium_driver():
         PyTest fixture to set up and tear down the Selenium WebDriver.
     """
     # Instantiate the web driver for Chrome
-    driver = webdriver.Chrome()
+    chrome_opt = webdriver.ChromeOptions()
+    chrome_opt.add_argument("--guest")
+    driver = webdriver.Chrome(chrome_opt)
     driver.maximize_window()
+    driver.delete_all_cookies()
     driver.implicitly_wait(3)  # Implicit wait to handle timing issues
+
 
     # Yield the driver instance for use in tests
     yield driver
